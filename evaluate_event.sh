e784b1445ffdc2bdb483c05682f14d4616d0d905
Long Trieu <longtrieujp@gmail.com>
2019-11-27 17:46:19 +0900

    update readme

--- evaluate_event.sh
+++ evaluate_event.sh
@@ -2,6 +2,7 @@
 
 refdir=data/corpora/CG13/dev/
 preddir=results/cg-predict-dev/ev-ann/
-scorepath=results/cg-predict-dev/ev-score.txt
+#scorepath=results/cg-predict-dev/ev-score.txt
 
-python scripts/eval-EV-CG13.py -r $refdir -d $preddir -sp > $scorepath
+python scripts/eval-EV-CG13.py -r $refdir -d $preddir -sp
+#python scripts/eval-EV-CG13.py -r $refdir -d $preddir -sp > $scorepath
