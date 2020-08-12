26cc54d9ab6851be7e637935715d43e172c06d7b
Long Trieu <longtrieujp@gmail.com>
2020-08-12 15:23:30 +0900

    fix prediction script

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 CORPUS_NAME=$1 # cg, ge11, pc, etc
 TASK=$2 # predict, eval
 DEV_TEST=$3 # predict for dev, test sets
+GOLD_E2E=$4 # gold, e2e
 
 experiment_dir="experiments/$CORPUS_NAME"
 log_dir="$experiment_dir/logs"
@@ -12,9 +13,6 @@ mkdir -p $log_dir
 if [ "$TASK" = "predict" ]; then
     echo "Predict:"
 
-    GOLD_E2E=$5 # gold, e2e
-    DEV_TEST=$4
-
     # predict
     nohup python -u predict.py --yaml $experiment_dir/configs/$TASK-$GOLD_E2E-$DEV_TEST.yaml >> $log_dir/$TASK-$GOLD_E2E-$DEV_TEST.log &
 
@@ -22,9 +20,6 @@ if [ "$TASK" = "predict" ]; then
 elif [ "$TASK" = "eval" ]; then
 
     echo "Evaluate:"
-
-    GOLD_E2E=$5 # gold, e2e
-    DEV_TEST=$4
 
     # paths
     REFDIR="data/corpora/$CORPUS_NAME/$DEV_TEST/" # reference gold data
