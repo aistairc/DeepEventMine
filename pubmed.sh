9a3e159419d949a66daa29348f94ffe7341229b0
Long Trieu <longtrieujp@gmail.com>
2020-08-17 17:19:56 +0900

    update pubmed

--- pubmed.sh
+++ pubmed.sh
@@ -2,13 +2,34 @@
 
 TASK=$1
 
-# pubmed_parser
+# Get Text from PubMed ID & PMC ID
 if [ "$TASK" = "pmids" ]; then
     echo "Get text from PubMed ID and PMC ID list"
 
     PMIDS="data/my-pubmed/my-pmid.txt"
     TEXT_DIR="data/my-pubmed/original_text/"
-    python pubmed/pubmed2text.py $PMIDS $TEXT_DIR
+    python pubmed/pubmed2texts.py $TASK $PMIDS $TEXT_DIR
+
+# Get Text from PubMed ID
+elif [ "$TASK" = "pmid" ]; then
+    echo "Get text from PubMed ID"
+
+    PMID=$2
+
+    TEXT_DIR="data/my-pubmed/original_text/"
+    python pubmed/pubmed2texts.py $TASK $PMID $TEXT_DIR
+
+fi
+
+# Get Text from PMC ID
+elif [ "$TASK" = "pmcid" ]; then
+    echo "Get text from PMC ID"
+
+    PMCID=$2
+
+    TEXT_DIR="data/my-pubmed/original_text/"
+    python pubmed/pubmed2texts.py $TASK $PMCID $TEXT_DIR
+
 fi
 
 echo "Done!"
--- pubmed/pubmed2text.py
+++ pubmed/pubmed2text.py
@@ -8,7 +8,7 @@ sys.path.insert(0, '.')
 from pubmed import med2text
 
 
-def pmid2text(pmid_path, textdir):
+def pmids2text(pmid_path, textdir):
     # read pmid list
     pmid_list = []
     pmcid_list = []
@@ -68,6 +68,59 @@ def pmid2text(pmid_path, textdir):
     return
 
 
+def pmid2text(pmid, textdir):
+    if not os.path.exists(textdir):
+        os.makedirs(textdir)
+
+    # get text given each PMID, write to file
+    print(pmid)
+    title, abstract = med2text.pmid2text(pmid)
+
+    if len(title) > 0:
+        with open(os.path.join(textdir, ''.join(['PMID-', pmid, '.txt'])), 'w') as fo:
+            fo.write(title)
+            fo.write('\n\n')
+            fo.write(abstract)
+        print('Done', pmid)
+
+    return
+
+
+def pmcid2text(pmcid, textdir):
+    if not os.path.exists(textdir):
+        os.makedirs(textdir)
+
+    try:
+        print(pmcid)
+        title, abstract, content = med2text.pmc2text(pmcid)
+        if len(title) > 0:
+            with open(os.path.join(textdir, ''.join(['PMC-', pmcid.replace('PMC', ''), '.txt'])), 'w') as fo:
+                fo.write(title)
+                fo.write('\n\n')
+                fo.write(abstract)
+                fo.write('\n\n')
+                fo.write(content)
+            print('Done', pmcid)
+
+    except urllib3.exceptions.ProtocolError as error:
+        print('Protocol Error', pmcid)
+    except requests.exceptions.ConnectionError as error:
+        print('Connection Error', pmcid)
+    except requests.exceptions.HTTPError as error:
+        print('HTTP Error', pmcid)
+
+
 if __name__ == '__main__':
     # pmid2text('../data/my-pubmed/pmid.txt', '../data/my-pubmed/original_text/')
-    pmid2text(sys.argv[1], sys.argv[2])
+
+    option = sys.argv[1]
+
+    # pubmed id list
+    if option == 'pmids':
+        pmids2text(sys.argv[2], sys.argv[3])
+
+    elif option == 'pmid':
+        pmid2text(sys.argv[2], sys.argv[3])
+
+    elif option == 'pmcid':
+        pmcid2text(sys.argv[2], sys.argv[3])
